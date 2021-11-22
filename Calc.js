75d2b1dd8f3b0215f1edf116c67509e9fc5448b5
jasper-steenhuis <steenhuisjasper1@gmail.com>
2021-11-22 23:26:37 +0100

    modified to add numbers to screen on button press

--- Calc.js
+++ Calc.js
@@ -1,7 +1,15 @@
-let arr = ["one", "two", "three"];
-function getMultipleElements(arr){
-    for(i=0; i<arr.length; i++){
-        console.log(document.getElementById(arr[i]));
-    }
+var input = document.getElementById("screen-content");
+var numbers = document.querySelectorAll(".keys-container button");
+var operator = document.querySelectorAll(".symbols button");
+input.innerHTML = " ";
+for(var i=0; i<numbers.length; i++){
+    var str = " ";
+
+    str = numbers[i].innerHTML;
+
+    numbers[i].addEventListener("click",function(e){
+        var currentString = input.innerHTML;
+        input.innerHTML += e.target.innerHTML;
+    });
+
 }
-getMultipleElements(arr);
